02264e5ae622f8c7934c2f38b130a3cad92c0f2c
dkumar <deepak.kumar.contractor@move.com>
2019-07-22 18:42:36 +0530

    Added area, minutestoyearsdays challenge

--- src/coding/challenge/Main.java
+++ src/coding/challenge/Main.java
@@ -8,5 +8,10 @@ public class Main {
         System.out.println("Is 1999 a leap year? " + LeapYear.isLeapYear(1999));
         System.out.println("Are two decimal numbers (6.75332 & 6.75312) equal to three decimal points? " + DecimalComparator.areEqualByThreeDecimalPlaces(3.176, 3.175));
         System.out.println("has teen? "+ TeenNumberCheck.hasTeen(78,39,17));
+        System.out.println("Area of circle " + AreaCalculator.area(7) );
+        System.out.println("Area of rectangle " + AreaCalculator.area(45, 50) );
+        MinutesToYearsDays.printYearsAndDays(1051200);
+
+
     }
 }
