34c865c6c71ab55eeb5f6afb601a16c9dabdbcfd
dkumar <deepak.kumar.contractor@move.com>
2019-07-22 18:15:20 +0530

    added overloading

--- src/com/company/OverloadingChallenge.java
+++ src/com/company/OverloadingChallenge.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class OverloadingChallenge {
+    public static void main (String[] args){
+        System.out.println("Inches to centimeters "+ calcFeetAndInchesToCentimeters(6));
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int feet, int inches){
+        double centi;
+        if((feet>=0)&&(inches>=0 && inches<=12)){
+            centi =  (30.48*feet + inches*2.54);
+            return centi;
+        }
+        return -1;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int inches){
+        if(inches>=0){
+            int feet = inches/12;
+            inches = inches%12;
+            return calcFeetAndInchesToCentimeters(feet,inches);
+        }
+        return -1;
+    }
+
 }
--- src/com/company/SecondsAndMinutes.java
+++ src/com/company/SecondsAndMinutes.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class SecondsAndMinutes {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";  //constant declaration
+    public static void main(String[] args){
+       System.out.println(getDurationString(3660));
+        System.out.println(getDurationString(61,128));
+    }
+    public static String getDurationString(int minutes, int seconds){
+        if(minutes<0 ||seconds<0 ||seconds>59){
+            return INVALID_VALUE_MESSAGE;
+        }
+        int hour = minutes/60;
+        minutes = minutes%60;
+        String time = hour + "h "+minutes+"m "+seconds+"s" ;
+        return time;
+    }
+
+    public static String getDurationString(int seconds){
+        if(seconds<0){
+            return INVALID_VALUE_MESSAGE;
+        }
+        int minutes = seconds/60;
+        seconds = seconds%60;
+        return getDurationString(minutes, seconds);
+    }
+
 }
