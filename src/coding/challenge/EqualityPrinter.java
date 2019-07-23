b5174cbbf98fdc9d7bc2876faed84d790a8e0463
dkumar <deepak.kumar.contractor@move.com>
2019-07-23 12:42:21 +0530

    Added playing cat challenge, equality printer

---
+++ src/coding/challenge/EqualityPrinter.java
@@ -0,0 +1,17 @@
+package coding.challenge;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class EqualityPrinter {
+    public static void printEqual(int para1, int para2, int para3){
+        if(para1<0||para2<0||para3<0){
+            System.out.println("Invalid Value");
+        }else if(para1==para2 && para2==para3){
+            System.out.println("All numbers are equal");
+        }else if(para1!=para2 && para2!=para3 && para3!=para1){
+            System.out.println("All numbers are different");
+        }else{
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
--- src/coding/challenge/Main.java
+++ src/coding/challenge/Main.java
@@ -2,6 +2,7 @@ package coding.challenge;
 
 public class Main {
     public static void main(String[] args) {
+        //called all challenges one by one
         SpeedConverter.printConversion(95.75);
         MegaBytesConverter.printMegaBytesAndKiloBytes(14000);
         System.out.println("Barking Dog::::::" + BarkingDog.shouldWakeUp(true, 7));
@@ -11,7 +12,7 @@ public class Main {
         System.out.println("Area of circle " + AreaCalculator.area(7) );
         System.out.println("Area of rectangle " + AreaCalculator.area(45, 50) );
         MinutesToYearsDays.printYearsAndDays(1051200);
-
-
+        EqualityPrinter.printEqual(1,1,1);
+        System.out.println("Is cat playing? " + PlayingCat.isCatPlaying(true, 41));
     }
 }
