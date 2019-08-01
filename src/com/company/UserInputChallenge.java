e8b44aecfa96f2e398035b8fbd223c762ab0eab3
dkumar <deepak.kumar.contractor@move.com>
2019-08-01 12:27:36 +0530

    updated challenges

---
+++ src/com/company/UserInputChallenge.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class UserInputChallenge {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n= 1;
+        int sum = 0;
+        while(n<=10){
+            System.out.println("Enter the number #"+n);
+            int num = scanner.nextInt();
+            sum = sum+num;
+            n++;
+        }
+
+        System.out.println("Sum of ten numbers "+sum);
+    }
+}
